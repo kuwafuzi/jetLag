f184b57a294171f3537803541b57e8cbc48c40a7
kato Yusuke <81180591+kuwafuzi@users.noreply.github.com>
2022-09-24 02:16:42 +0900

    Update jetLag.java

--- jetLag.java
+++ jetLag.java
@@ -16,10 +16,39 @@ public class jetLag {
             int sum = 0 + depart + flyingTime + (24 - arrive);
             list.add(sum);
         }
-
+        
+        // output min & max in the list
         int min = Collections.min(list);
         int max = Collections.max(list);
         System.out.println(min);
         System.out.println(max);
     }
 }
+//------------------------------------------------//
+// 入力例1
+// 3
+// 7 5 12
+// 10 6 20
+// 12 3 8
+
+// 出力例1
+// 20
+// 31
+//------------
+// 入力例2
+// 2
+// 1 1 23
+// 23 1 1
+    
+// 出力例2
+// 3
+// 47
+//------------  
+// 入力例3
+// 1
+// 5 4 20
+
+// 出力例3
+// 13
+// 13
+
